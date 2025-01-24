72fa8fbdfbe002ec7954b1973705e047ccadffa4
Carlos Filipe <thecarlosfilipe@gmail.com>
2025-01-23 23:21:37 -0300

    Add updateCategory endpoint and route

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -31,3 +31,33 @@ exports.getCategories = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 }
+
+exports.updateCategory = async (req, res) => {
+  try {
+
+    if (!await prisma.category.findUnique({ where: { id: parseInt(req.params.id) } })) {
+      return res.status(404).json({ error: `A categoria [${req.params.id}] não foi encontrada!` });
+    }
+
+    if (!req.body.name) {
+      return res.status(422).json({ error: 'O nome é obrigatório!' });
+    }
+
+    if (await prisma.category.findUnique({ where: { name: req.body.name } })) {
+      return res.status(409).json({ error: `A categoria [${req.body.name}] já existe!` });
+    }
+
+    const updatedCategory = await prisma.category.update({
+      data: {
+        name: req.body.name,
+      },
+      where: {
+        id: parseInt(req.params.id),
+      }
+    });
+
+    return res.status(200).json(updatedCategory);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+} 
--- src/routes/categoryRoute.js
+++ src/routes/categoryRoute.js
@@ -3,5 +3,6 @@ const categoryController = require('../controllers/categoryController');
 
 router.post('/', categoryController.createCategory);
 router.get('/', categoryController.getCategories);
+router.put('/:id', categoryController.updateCategory);
 
 module.exports = router;
